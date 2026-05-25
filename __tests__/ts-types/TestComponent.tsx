aeaf326b7eca3812e4b9c28415d8047145332250
mrazauskas <tom@mrazauskas.de>
2026-05-25 14:36:43 +0300

    chore: update tstyche to v7.2
    Signed-off-by: mrazauskas <tom@mrazauskas.de>

--- __tests__/ts-types/TestComponent.tsx
+++ __tests__/ts-types/TestComponent.tsx
@@ -3,9 +3,9 @@ import { expect } from 'tstyche';
 import TestComponent from '../../jest/TestComponent';
 import themed from '../../src';
 
-expect(<TestComponent />).type.toRaiseError(2741);
+expect(TestComponent).type.not.toAcceptProps({})
 
 const Themed = themed(TestComponent, 'Themed');
 
-expect(<Themed badKey="value" />).type.toRaiseError(2322);
-expect(<Themed goodKey="good" />).type.not.toRaiseError();
+expect(Themed).type.not.toAcceptProps({ badKey: "value "})
+expect(Themed).type.toAcceptProps({ goodKey: "value" })
